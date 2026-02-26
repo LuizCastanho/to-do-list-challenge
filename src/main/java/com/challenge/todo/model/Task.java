ff2dfc239285f705a2fae7bacecb3d854d2dfb0c
Luiz Castanho <luiz.castanho92@gmail.com>
2026-02-25 21:34:50 -0300

    second commit

---
+++ src/main/java/com/challenge/todo/model/Task.java
@@ -0,0 +1,62 @@
+package com.challenge.todo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Boolean done;
+
+    private Integer priority;
+
+    // Construtor vazio obrigatório pelo JPA
+    public Task() {
+    }
+
+    //Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setNome(String name) {
+        this.name = name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+}
